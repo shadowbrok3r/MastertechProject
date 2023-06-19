58ba2888fd1c942cfc129328088bfcd1dfa2dea4
Logan <darkmage79@gmail.com>
2023-06-19 17:18:14 -0600

    further improving ui, added egui_extras

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use eframe::egui;
-//use egui::Visuals;
+use egui::{Stroke, style::Spacing, Margin};
+use egui_extras::{Size, StripBuilder};
 
 fn main() {
     let native_options = eframe::NativeOptions::default();
@@ -18,10 +19,10 @@ struct MastertechApp {
 impl MastertechApp {
     fn new(cc: &eframe::CreationContext<'_>) -> Self {
         // Get current context style
-        let style = (cc.egui_ctx.style()).clone();
-        //let colors = egui::Color32::from_rgb(0,0,0);
-        
-        
+        //let mut style = (cc.egui_ctx.style()).clone(); //let mut style: egui::Style = (*ctx.style()).clone();
+        //cc.egui_ctx.set_style(egui::Style::)
+        //style.spacing.item_spacing = egui::vec2(10.0, 20.0);
+        //ctx.set_style(style);
         //ctx.set_visuals(egui::Visuals::dark()); 
         // Customize egui here with cc.egui_ctx.set_fonts and cc.egui_ctx.set_visuals.
         // Restore app state using cc.storage (requires the "persistence" feature).
@@ -33,32 +34,95 @@ impl MastertechApp {
 
 impl eframe::App for MastertechApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        let mut style = (*ctx.style()).clone();
+
+        style.spacing.window_margin = Margin {
+            left: 5.,
+            right: 5.,
+            top: 300.,
+            bottom: 5.,
+        };
+
+        ctx.set_style(style);
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.visuals_mut().override_text_color = Some(egui::Color32::from_rgb(144, 238, 144));
-            ui.horizontal(|ui| {
-                // Add tabs
-                if ui.selectable_label(self.active_tab == 0, "Main").clicked() {
-                    self.active_tab = 0;
-                }else if ui.selectable_label(self.active_tab == 1, "Scripts").clicked() {
-                    self.active_tab = 1;
-                }
-                // Add more tabs as needed
+
+                ui.visuals_mut().override_text_color = Some(egui::Color32::from_rgb(144, 238, 144));
+                ui.horizontal(|ui| {
+
+                    // Add tabs
+                    if ui.selectable_label(self.active_tab == 0, "Main").clicked() {
+                        self.active_tab = 0;
+                    }else if ui.selectable_label(self.active_tab == 1, "Scripts").clicked() {
+                        self.active_tab = 1;
+                    }
+                
             });
 
+
             if self.active_tab == 0 {
-                // Add text fields
-                ui.with_layout(Layout::left_to_right, Align::left_top)(|ui| {
-                    ui.columns(2, |cols| {
-                        cols[0].add(egui::TextEdit::singleline(&mut self.so_number).hint_text("SO#"));
-                        cols[1].add(egui::TextEdit::singleline(&mut self.customer_name).hint_text("Customer Name"));
+                ui.vertical(|ui| {
+                    StripBuilder::new(ui)
+                        .size(Size::remainder()) // top cell
+                        .size(Size::remainder())
+                        .vertical(|mut strip| {
+
+
+
+
+                            strip.cell(|ui| {
+
+                                ui.painter().rect_filled(
+                                    ui.available_rect_before_wrap(),
+                                    0.0,
+                                    egui::Color32::from_rgba_premultiplied(23, 194, 145, 1)
+                                );
+
+                                ui.horizontal(|ui| {
+                                    ui.add(egui::TextEdit::singleline(&mut self.so_number).hint_text("SO#").char_limit(8)
+                                    .vertical_align(egui::Align::Center)
+                                    .desired_width(130.0));
+
+                                    ui.add(egui::TextEdit::singleline(&mut self.customer_name).hint_text("Customer Name")
+                                    .desired_width(130.0));
+                                });
+
+                                ui.horizontal(|ui| {
+                                    ui.add(egui::TextEdit::singleline(&mut self.phone1).hint_text("Phone Number 1")
+                                    .desired_width(130.0));
+                                
+                                    ui.add(egui::TextEdit::singleline(&mut self.phone2).hint_text("Phone Number 2")
+                                    .desired_width(130.0));      
+                                });
+                            });
+
+
+
+
+
+                            strip.strip(|builder|{
+                                builder.sizes(Size::remainder(), 3)
+                                .vertical(|mut strip| {
+
+                                    strip.cell(|ui|{
+                                        ui.painter().rect_stroke(
+                                            ui.available_rect_before_wrap(),
+                                            0.0,
+                                            egui::Stroke {width: 5.0, color: egui::Color32::RED}
+                                        );
+
+                                    });
+                                    strip.cell(|ui| {
+                                        ui.vertical_centered(|ui| {
+                                            ui.add(egui::TextEdit::singleline(&mut self.phone2).hint_text("TEST")
+                                            .desired_width(130.0));     
+                                        });
+                                    });
+                                });
+                            });
+                        });
                     });
-                    ui.columns(2, |cols| {
-                        cols[0].add(egui::TextEdit::singleline(&mut self.phone1).hint_text("Phone Number 1"));
-                        cols[1].add(egui::TextEdit::singleline(&mut self.phone2).hint_text("Phone Number 2"));
-                    });
-                });
-            }else if self.active_tab == 1 {
-                // Add text fields
+
+            }if self.active_tab == 1 {
                 ui.vertical_centered(|ui| {
                     ui.columns(2, |cols| {
                         cols[0].add(egui::TextEdit::singleline(&mut self.so_number).hint_text("SO#"));
