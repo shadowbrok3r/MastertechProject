8b91c91cd8bee67df7c3dc6cc4ef900ab90e31ad
Logan <darkmage79@gmail.com>
2023-06-23 11:23:39 -0600

    fixed the weird combo box alignment issue

--- src/main.rs
+++ src/main.rs
@@ -26,8 +26,9 @@ pub struct MastertechApp {
     customer_name: String,
     phone1: String,
     phone2: String,
-    Salesman_cBox: Salesman,
-    Techs_cBox: Techs,
+    salesman_cbox: Salesman,
+    techs_cbox: Techs,
+    checkin_notes: String,
 
     //////////////////////////////////////////
     /*          Widgets and UI elements     */
@@ -57,8 +58,9 @@ impl Default for MastertechApp {
             customer_name: "".to_string(),
             phone1: "".to_string(),
             phone2: "".to_string(),
-            Salesman_cBox: Salesman::Jake,
-            Techs_cBox: Techs::Logan,
+            salesman_cbox: Salesman::Jake,
+            techs_cbox: Techs::Logan,
+            checkin_notes: "".to_string(),
 
             //////////////////////////////////////////
             /*          Widgets and UI elements     */
@@ -94,7 +96,7 @@ impl eframe::App for MastertechApp {
         
         let mut style = (*ctx.style()).clone();
         style.spacing.combo_width = self.widget_size;
-        style.spacing.button_padding = vec2(8.0, 8.0);
+        //style.spacing.button_padding = vec2(8.0, 8.0); //This is causing weirdness with only the Tech comboBox...
         style.visuals.window_stroke = Stroke{width:1.0, color: self.purple_color};
         style.visuals.dark_mode = true;
         style.visuals.window_fill = self.window_fill_color;
@@ -111,12 +113,16 @@ impl eframe::App for MastertechApp {
             ScrollArea::vertical()
             .stick_to_bottom(true)
             .show(ui, |ui| {
+                ui.painter().rect_filled(ui.available_rect_before_wrap(),10.0,self.strip_bg_color);
+                ui.painter().rect_stroke(ui.available_rect_before_wrap(),10.0, self.purple_stroke);
+                ui.horizontal(|ui|{
 
+                });
             });
         });
 
         CentralPanel::default()
-            //.frame(Frame::inner_margin(self.default_frame, self.default_margins))
+            .frame(Frame::inner_margin(self.default_frame, self.default_margins))
             .show(ctx, |ui| {
                 ui.add_space(10.0);
                 ui.visuals_mut().override_text_color = Some(self.text_color);
@@ -134,30 +140,29 @@ impl eframe::App for MastertechApp {
             if self.active_tab == 0 {
                 StripBuilder::new(ui)
                     .size(Size::relative(0.5))// top strip
-                    .size(Size::relative(0.25)) // bottom strip
-                    .vertical(|mut strip| {
+                    .size(Size::relative(0.5)) // bottom strip
+                    .horizontal(|mut strip| {
                         strip.strip(|builder| {
-                            builder.sizes(Size::remainder(), 2).horizontal(|mut strip| {
-                                strip.cell(|ui| {
-                                    
-                                    // Background color for Top Left cell
+                            builder.sizes(Size::remainder(), 1).horizontal(|mut strip| {
+                                strip.cell(|ui|{
                                     ui.painter().rect_filled(ui.available_rect_before_wrap(),10.0,self.strip_bg_color);
                                     ui.painter().rect_stroke(ui.available_rect_before_wrap(),10.0, self.purple_stroke);
                                     
-                                    ui.vertical(|ui| {ui.add_space(3.0);});
 
-                                    ui.horizontal(|ui| {
-                                        ui.add_space(10.0);
-                                        
+                                    ui.vertical(|ui| {ui.add_space(3.0);}); // leave some margin above the textEdits
+
+                                    
+                                    ui.horizontal(|ui|{
+                                        ui.add_space(15.0);
                                         ui.add(TextEdit::singleline(&mut self.so_number)
-                                        .hint_text("SO#").char_limit(8).vertical_align(Align::Center).desired_width(self.widget_size));
+                                        .hint_text("SO#").char_limit(8).desired_width(self.widget_size));
                                         
                                         ui.add(TextEdit::singleline(&mut self.customer_name)
                                         .hint_text("Customer Name").desired_width(self.widget_size));
                                     });
         
                                     ui.horizontal(|ui| {
-                                        ui.add_space(10.0);
+                                        ui.add_space(15.0);
 
                                         ui.add(TextEdit::singleline(&mut self.phone1)
                                         .hint_text("Phone Number 1").desired_width(self.widget_size));
@@ -168,34 +173,38 @@ impl eframe::App for MastertechApp {
 
                                     // Salesman and Tech ComboBoxes
                                     ui.horizontal(|ui|{
-                                        ui.add_space(10.0);
+                                        ui.add_space(15.0);
 
-                                        ComboBox::new("salesman_cbox", "")
-                                        .selected_text(format!("{:?}", self.Salesman_cBox))
+                                        ComboBox::from_id_source("salesman_cbox")
+                                        .selected_text(format!("{:?}", self.salesman_cbox))
                                         .show_ui(ui, |ui| {
-                                            ui.selectable_value(&mut self.Salesman_cBox, Salesman::Jake, "Jake");
-                                            ui.selectable_value(&mut self.Salesman_cBox, Salesman::Danny, "Danny");
+                                            ui.selectable_value(&mut self.salesman_cbox, Salesman::Jake, "Jake");
+                                            ui.selectable_value(&mut self.salesman_cbox, Salesman::Danny, "Danny");
                                         });
 
-                                        ComboBox::new("techs_cbox", "")
-                                        .selected_text(format!("{:?}", self.Techs_cBox))
+                                        ComboBox::from_id_source("techs_cbox")
+                                        .selected_text(format!("{:?}", self.techs_cbox))
                                         .show_ui(ui, |ui| {
-                                            ui.selectable_value(&mut self.Techs_cBox, Techs::Logan, "Logan");
-                                            ui.selectable_value(&mut self.Techs_cBox, Techs::Bread, "Bread");
-                                            ui.selectable_value(&mut self.Techs_cBox, Techs::Taco, "Taco");
+                                            ui.selectable_value(&mut self.techs_cbox, Techs::Logan, "Logan");
+                                            ui.selectable_value(&mut self.techs_cbox, Techs::Bread, "Bread");
+                                            ui.selectable_value(&mut self.techs_cbox, Techs::Taco, "Taco");
                                         });
                                     });
+                                    ui.horizontal_top(|ui|{
+                                        ui.add_space(15.0);
+                                        ui.add(TextEdit::multiline(&mut self.checkin_notes)
+                                        .hint_text("Checkin Notes").desired_rows(20).desired_width(self.widget_size * 2.0+8.0)); // .desired_width(self.widget_size));
+                                    });
+
                                 });
+                            });
+                        });
+                        strip.strip(|builder| {
+                            builder.sizes(Size::remainder(), 1).horizontal(|mut strip| {
+                                strip.cell(|ui|{
+                                    ui.painter().rect_filled(ui.available_rect_before_wrap(),10.0,self.strip_bg_color);
+                                    ui.painter().rect_stroke(ui.available_rect_before_wrap(),10.0, self.purple_stroke);
 
-                                //T
-                                strip.strip(|builder| {
-                                    builder.sizes(Size::remainder(), 1).horizontal(|mut strip| {
-                                        strip.cell(|ui| {
-                                            // Background color for Top Left cell
-                                            ui.painter().rect_filled(ui.available_rect_before_wrap(),10.0,self.strip_bg_color);
-                                            ui.painter().rect_stroke(ui.available_rect_before_wrap(),10.0, self.purple_stroke);
-                                        });
-                                    });
                                 });
                             });
                         });
