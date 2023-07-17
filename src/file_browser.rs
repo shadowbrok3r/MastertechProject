00aaeecd24f6fbd2a27aea4add29a048ac36f3f9
Logan <darkmage79@gmail.com>
2023-07-17 17:19:42 -0600

    changed whole file browser structure

--- src/file_browser.rs
+++ src/file_browser.rs
@@ -2,12 +2,16 @@ use std::io::{stdin, stdout, Write};
 use std::path::PathBuf;
 use tokio::{task, fs};
 use walkdir::WalkDir;
-use tokio::sync::mpsc::{UnboundedSender, unbounded_channel, UnboundedReceiver};
-use std::io::{Error, Result};
+use tokio::{sync::mpsc::{UnboundedSender, unbounded_channel, UnboundedReceiver}};
+use std::{env, io::{Error, Result}};
 use thiserror::Error;
-use std::future::Future;
-use std::pin::Pin;
-use async_recursion::async_recursion;
+
+use egui::{
+    vec2, Align2, Context, Key, Layout, Pos2, RichText, ScrollArea, TextEdit, Ui, Vec2, Window,
+};
+
+/// Function that returns `true` if the path is accepted.
+//pub type Filter = Box<dyn Fn(&PathBuf) -> bool + Send + Sync + 'static>;
 
 #[derive(Debug)]
 pub enum Response {
@@ -33,15 +37,6 @@ pub struct Directory {
     pub files: Vec<PathBuf>,
 }
 
-#[derive(Debug)]
-pub enum Command {
-    Copy(PathBuf, PathBuf),
-    Move(PathBuf, PathBuf),
-    Delete(PathBuf),
-    Rename(PathBuf, PathBuf),
-    ListDir(PathBuf, usize),
-}
-
 pub struct CommandControl {
     command_sender: UnboundedSender<Command>,
     response_receiver: UnboundedReceiver<Response>,
@@ -53,7 +48,7 @@ impl CommandControl {
         let (response_sender, response_receiver) = unbounded_channel::<Response>();
 
         tokio::spawn(async move {
-            file_browsing(command_receiver, response_sender).await;
+            //file_browsing(command_receiver, response_sender).await;
         });
 
         Self {
@@ -71,73 +66,324 @@ impl CommandControl {
     }
 }
 
-pub async fn file_browsing(mut command_receiver: UnboundedReceiver<Command>, response_sender: UnboundedSender<Response>) {
-    while let Some(command) = command_receiver.recv().await {
-        match command {
-            Command::ListDir(path, depth) => {
-                match read_directory_boxed(path.clone(), depth).await {
-                    Ok(directory) => {
-                        let _ = response_sender.send(Response::DirectoryListing(directory));
-                    },
-                    Err(err) => {
-                        let _ = response_sender.send(Response::Error(FileBrowserError::Other(err.to_string())));
+#[derive(Debug)]
+pub enum Command {
+    Copy(PathBuf, PathBuf),
+    Move(PathBuf, PathBuf),
+    Delete(PathBuf),
+    Rename(PathBuf, PathBuf),
+    //ListDir(PathBuf, usize),
+
+    CreateDirectory,
+    Folder,
+    Refresh,
+    Select(PathBuf),
+    UpDirectory,
+}
+
+pub struct FileBrowser {
+    path: PathBuf, /// Current opened path.
+    path_edit: String, /// Editable field with path.
+    selected_file: Option<PathBuf>, /// Selected file path
+    filename_edit: String, /// Editable field with filename.
+    files: core::result::Result<Vec<PathBuf>, Error>, /// Files in directory.
+    read_dirs_only: bool,
+    read_hidden_files: bool,
+    //filter: Option<Filter>,
+    rename: bool,
+    new_folder: bool,
+  
+    // Show hidden files on unix systems.
+    //#[cfg(unix)]
+    show_hidden: bool,
+}
+
+impl FileBrowser{
+    pub fn new() -> Self{
+        let mut path = env::current_dir().unwrap_or_default();
+        let mut filename_edit = String::new();
+
+        let path_edit = path.to_str().unwrap_or_default().to_string();
+
+        if path.is_file() {
+            filename_edit = get_file_name(&path).to_string();
+            path.pop();
+        }
+
+        Self {
+            path,
+            path_edit,
+            selected_file: None,
+            filename_edit,
+            files: Ok(Vec::new()),
+            read_dirs_only: false,
+            read_hidden_files: false,
+
+            //filter: None,
+            rename: true,
+            new_folder: true,
+    
+            #[cfg(unix)]
+            show_hidden: false,
+          }
+    }
+
+
+
+    pub async fn show(&mut self, ctx: Context){
+
+        let mut command: Option<Command> = None;
+        ui.horizontal(|ui| {
+            ui.add_enabled_ui(self.path.parent().is_some(), |ui| {
+                let response = ui.button("⬆").on_hover_text("Parent Folder");
+                if response.clicked() {
+                    command = Some(Command::UpDirectory);
+                }
+            });
+
+            ui.with_layout(Layout::right_to_left(egui::Align::Center), |ui| {
+            
+                let response = ui.button("⟲").on_hover_text("Refresh");
+                if response.clicked() {
+                    command = Some(Command::Refresh);
+                }
+
+                let response = ui.add_sized(
+                ui.available_size(),
+                TextEdit::singleline(&mut self.path_edit).cursor_at_end(true),
+                );
+
+                if response.lost_focus() {
+                    let path = PathBuf::from(&self.path_edit);
+                    //command = Some(Command::Open(path));
+                };
+
+            });
+        });
+        
+        ui.horizontal_top(|ui| {
+            ui.checkbox(&mut self.read_dirs_only, "Show Directories ONLY");
+            ui.checkbox(&mut self.read_hidden_files, "Show hidden files");
+        });
+
+        ui.add_space(ui.spacing().item_spacing.y);
+
+        let scroll_area = ScrollArea::new([true, true])
+            .id_source("file_browser_scroll")
+            .auto_shrink([false, false]);
+ 
+        scroll_area.show_rows(
+        ui,
+        ui.text_style_height(&egui::TextStyle::Body),
+self.files.as_ref().map_or(0, |files| files.len()),
+|ui, range| match self.files.as_ref() {
+                Ok(files) => {
+                    ui.with_layout(ui.layout().with_cross_justify(true), |ui| {
+                        for path in files[range].iter() {
+                            let label = match path.is_dir() {
+                                true => "🗀 ",
+                                false => "🗋 ",
+                            }.to_string() + get_file_name(path);
+            
+                            let is_selected = Some(path) == self.selected_file.as_ref();
+                            let selectable_label = ui.selectable_label(is_selected, label);
+                            if selectable_label.clicked() {
+                                command = Some(Command::Select(path.clone()));
+                            }
+            
+                            if selectable_label.double_clicked() {
+                                // command = Some(match self.dialog_type == DialogType::SaveFile {
+                                // true => match path.is_dir() {
+                                //     true => Command::OpenSelected,
+                                //     false => Command::Save(path.clone()),
+                                // },
+                                // false => Command::Open(path.clone()),
+                                // });
+                            }
+                        }
+                    }).response
+                }
+                Err(e) => ui.label(e.to_string()),
+            },
+        );
+
+
+        ui.add_space(ui.spacing().item_spacing.y * 2.0);
+        ui.horizontal(|ui| {
+            ui.label("File:");
+            ui.with_layout(Layout::right_to_left(egui::Align::Center), |ui| {
+                if self.new_folder && ui.button("New Folder").clicked() {
+                    command = Some(Command::CreateDirectory);
+                }
+
+                if self.rename {
+                    ui.add_enabled_ui(self.can_rename(), |ui| {
+                        if ui.button("Rename").clicked() {
+                        if let Some(from) = self.selected_file.clone() {
+                            let to = from.with_file_name(&self.filename_edit);
+                            command = Some(Command::Rename(from, to));
+                        }
+                        }
+                    });
+                }
+
+                let result = ui.add_sized(
+                ui.available_size(),
+                TextEdit::singleline(&mut self.filename_edit),
+                );
+
+                if result.lost_focus()
+                && result
+                    .ctx
+                    .input(|state| state.key_pressed(egui::Key::Enter))
+                && !self.filename_edit.is_empty(){
+                    let path = self.path.join(&self.filename_edit);
+
+                }
+            });
+        });
+        
+        if let Some(command) = command {
+            match command {
+                Command::Select(file) => self.select(Some(file)),
+
+                Command::Folder => {
+                    //self.selected_file = Some(self.get_folder().to_owned());
+                }
+                
+                Command::Refresh => self.refresh(),
+
+                Command::UpDirectory => {
+                    if self.path.pop() {
+                        self.refresh();
                     }
                 }
-            },
-            Command::Copy(source, destination) => {
-                if let Err(err) = fs::copy(&source, &destination).await {
-                    let _ = response_sender.send(Response::Error(FileBrowserError::Io(err)));
-                } else {
-                    let _ = response_sender.send(Response::Success(format!("Successfully copied from {:?} to {:?}", source, destination)));
+
+                Command::CreateDirectory => {
+                    let mut path = self.path.clone();
+                    let name = match self.filename_edit.is_empty() {
+                        true => "New folder",
+                        false => &self.filename_edit,
+                    };
+                    path.push(name);
+                    match fs::create_dir(&path).await {
+                        Ok(_) => {
+                        self.refresh();
+                        self.select(Some(path));
+                        // TODO: scroll to selected?
+                        }
+                        Err(err) => println!("Error while creating directory: {err}"),
+                    }
                 }
-            },
-            Command::Move(source, destination) => {
-                if let Err(err) = fs::rename(&source, &destination).await {
-                    let _ = response_sender.send(Response::Error(FileBrowserError::Io(err)));
-                } else {
-                    let _ = response_sender.send(Response::Success(format!("Successfully moved from {:?} to {:?}", source, destination)));
-                }
-            },
-            Command::Delete(path) => {
-                if let Err(err) = fs::remove_dir_all(&path).await {
-                    let _ = response_sender.send(Response::Error(FileBrowserError::Io(err)));
-                } else {
-                    let _ = response_sender.send(Response::Success(format!("Successfully deleted {:?}", path)));
-                }
-            },
-            Command::Rename(source, destination) => {
-                if let Err(err) = fs::rename(&source, &destination).await {
-                    let _ = response_sender.send(Response::Error(FileBrowserError::Io(err)));
-                } else {
-                    let _ = response_sender.send(Response::Success(format!("Successfully renamed from {:?} to {:?}", source, destination)));
-                }
-            },
+                Command::Copy(source, destination) => {
+                    if let Err(err) = fs::copy(&source, &destination).await {
+                        //let _ = response_sender.send(Response::Error(FileBrowserError::Io(err)));
+                    } else {
+                        //let _ = response_sender.send(Response::Success(format!("Successfully copied from {:?} to {:?}", source, destination)));
+                    }
+                },
+                Command::Move(source, destination) => {
+                    if let Err(err) = fs::rename(&source, &destination).await {
+                        //let _ = response_sender.send(Response::Error(FileBrowserError::Io(err)));
+                    } else {
+                        //let _ = response_sender.send(Response::Success(format!("Successfully moved from {:?} to {:?}", source, destination)));
+                    }
+                },
+                Command::Delete(path) => {
+                    if let Err(err) = fs::remove_dir_all(&path).await {
+                        //let _ = response_sender.send(Response::Error(FileBrowserError::Io(err)));
+                    } else {
+                        //let _ = response_sender.send(Response::Success(format!("Successfully deleted {:?}", path)));
+                    }
+                },
+
+                // Command::Rename(source, destination) => {
+                //     if let Err(err) = fs::rename(&source, &destination).await {
+                //         let _ = response_sender.send(Response::Error(FileBrowserError::Io(err)));
+                //     } else {
+                //         let _ = response_sender.send(Response::Success(format!("Successfully renamed from {:?} to {:?}", source, destination)));
+                //     }
+                // },
+
+                Command::Rename(from, to) => match fs::rename(from, &to).await {
+                    Ok(_) => {
+                        self.refresh();
+                        self.select(Some(to));
+                    }
+                    Err(err) => println!("Error while renaming: {err}"),
+                },
+            };
         }
     }
-}
 
+    pub fn default_filename(mut self, filename: impl Into<String>) -> Self {
+        self.filename_edit = filename.into();
+        self
+    }
 
+    /// Resulting file path.
+    pub fn path(&self) -> Option<PathBuf> {
+        self.selected_file.clone()
+    }
 
+    fn refresh(&mut self) {
+        //read_directory_boxed
+        self.path_edit = String::from(self.path.to_str().unwrap_or_default());
+        self.select(None);
+    }
 
+    fn select(&mut self, file: Option<PathBuf>) {
+        self.filename_edit = match &file {
+            Some(path) => get_file_name(path).to_string(),
+            None => String::new(),
+        };
+        self.selected_file = file;
+    }
 
-pub fn read_directory_boxed(path: PathBuf, depth: usize) -> Pin<Box<dyn Future<Output = std::result::Result<Directory, Error>> + Send>> {
-    let path_clone = path.clone();
-    Box::pin(async move {
-        let mut children = Vec::new();
-        let mut files = Vec::new();
-        if depth > 0 {
-            let mut dir = fs::read_dir(path_clone).await?;
-            while let Some(child) = dir.next_entry().await? {
-                let child_path = child.path();
-                if child.metadata().await?.is_dir() {
-                    let child_dir = read_directory_boxed(child_path, depth - 1).await?;
-                    children.push(child_dir);
-                } else {
-                    files.push(child_path);
-                }
+    fn can_save(&self) -> bool {
+        self.selected_file.is_some() || !self.filename_edit.is_empty()
+    }
+
+    fn can_open(&self) -> bool {
+        self.selected_file.is_some()
+    }
+
+    fn can_rename(&self) -> bool {
+        if !self.filename_edit.is_empty() {
+            if let Some(file) = &self.selected_file {
+            return get_file_name(file) != self.filename_edit;
             }
         }
-        Ok(Directory { path, children, files })
-    })
+
+        false
+    }
+    
+    
+    // Set a function to filter shown files.
+    // pub fn filter(mut self, filter: Filter) -> Self {
+    //     self.filter = Some(filter);
+    //     self
+    // }
+    
+    // Returns true, if the file selection was confirmed.
+    // pub fn selected(&self) -> bool {
+        // self.state == State::Selected
+    // }
+
 }
 
+fn get_file_name(path: &PathBuf) -> &str {
+    #[cfg(windows)]
+    if path.is_dir() && is_drive_root(path) {
+      return path.to_str().unwrap_or_default();
+    }
+    path
+      .file_name()
+      .and_then(|name| name.to_str())
+      .unwrap_or_default()
+}
+   
+#[cfg(windows)]
+extern "C" {
+    pub fn GetLogicalDrives() -> u32;
+}
