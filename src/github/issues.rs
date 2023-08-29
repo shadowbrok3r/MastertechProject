c4043cd4a29f7c16e81ac7f5ad26420b6559882d
Shadowbroker <darkmage79@gmail.com>
2023-08-28 19:42:31 -0600

    moving computers

--- src/github/issues.rs
+++ src/github/issues.rs
@@ -1,6 +1,6 @@
 #![allow(non_snake_case)]
 #![cfg_attr(debug_assertions, allow(dead_code, unused_imports, unused_variables))]
-use reqwest::header::{Authorization, ACCEPT};
+use reqwest::header::{AUTHORIZATION, ACCEPT};
 use serde::{Deserialize, Serialize};
 use serde_json::*;
 use std::error::Error;
@@ -8,7 +8,7 @@ use std::error::Error;
 const TOKEN: &str = "Bearer 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";
 
 async fn create_new_issue(title: String, body: String, client: reqwest::Client)  
--> core::result::Result<Box<dyn Error>> {
+/* -> core::result::Result<Box<dyn Error>> */ {
 
     // Now you can use the method on the instance of ScaffoldRequestBuilder
     let params = serde_json::json!({
@@ -22,27 +22,27 @@ async fn create_new_issue(title: String, body: String, client: reqwest::Client)
 
     let response = client
         .post("https://api.github.com/repos/shadowbrok3r/Mastertech4.0/issues") //https://5dccaa60-8a54-47f1-8ff6-ce32034dd0f6.mock.pstmn.io
-        .header(Authorization, TOKEN)
+        .header(AUTHORIZATION, TOKEN)
         .header(ACCEPT, "application/vnd.github+json")
         .json(&params)
         .send()
         .await;
 
-    match response {
-        Ok(res) => {
-            if cfg!(debug_assertions){
+    // match response {
+    //     Ok(res) => {
+    //         if cfg!(debug_assertions){
                 
-                let raw_response = res.json().await?;
-                println!("raw resp: {raw_response:?}");
-                Ok(raw_response)
-            }else{
-                let json_response: GetTicketResponse = res.json().await?;
-                Ok(json_response)
-            }
-        },
-        Err(e) => {
-            println!("Boxed error: {e:?}");
-            Err(Box::new(e))
-        },
-    }
+    //             let raw_response = res.json().await;
+    //             println!("raw resp: {raw_response:?}");
+    //             Ok(raw_response)
+    //         }else{
+    //             let json_response = res.json().await;
+    //             Ok(json_response)
+    //         }
+    //     },
+    //     Err(e) => {
+    //         println!("Boxed error: {e:?}");
+    //         Err(Box::new(e))
+    //     },
+    // }
 }
