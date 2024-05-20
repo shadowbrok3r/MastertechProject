8d3bae591632888ed76f1b19a01ebdb043836925
Logan <darkmage79@gmail.com>
2024-05-19 22:36:26 -0600

    misc

--- build.rs
+++ build.rs
@@ -6,7 +6,7 @@ fn main() {
     {
         static_vcruntime::metabuild();
         println!("cargo:rerun-if-changed=MasterTech.rc");
-        embed_resource::compile("MasterTech.rc", embed_resource::NONE);
+        embed_resource::compile("src/assets/MasterTech.rc", embed_resource::NONE);
         // println!("cargo rustc -- -Ctarget-feature=+crt-static");
         // println!("cargo:rustc-link-lib=static=stdc++");
     }
