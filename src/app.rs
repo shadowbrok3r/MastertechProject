6a398f2bb7988384b5366d71920363cf40f9b4ab
Shadowbroker <darkmage79@gmail.com>
2024-05-19 23:22:12 -0600

    Update app.rs
    Stop yelling at me about cc…

--- src/app.rs
+++ src/app.rs
@@ -8,7 +8,7 @@ use crate::MtechServer;
 
 impl MtechServer {
     /// Called once before the first frame.
-    pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
+    pub fn new(_cc: &eframe::CreationContext<'_>) -> Self {
         // This is also where you can customize the look and feel of egui using
         // `cc.egui_ctx.set_visuals` and `cc.egui_ctx.set_fonts`.
 
