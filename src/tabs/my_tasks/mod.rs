3bf3def710e79dd85e0f2cd2e6ba2675facebb24
Shadowbroker <darkmage79@gmail.com>
2024-05-22 17:05:42 -0600

    got task layout wrap working

--- src/tabs/my_tasks/mod.rs
+++ src/tabs/my_tasks/mod.rs
@@ -1,22 +1,44 @@
-use egui::Ui;
+use crate::{app_state::MtechServerContext, database::methods::Displayable};
+use egui::{vec2, Layout, ScrollArea, Ui, Vec2};
 use log::info;
-
-use crate::{app_state::MtechServerContext, utilities::task_layout::TaskLayout};
-
 
 impl MtechServerContext{
     pub fn my_tasks(&mut self, ui: &mut Ui){ 
         ui.style_mut().spacing.button_padding = (4.0, 7.0).into();
-        ui.shrink_width_to_current();
-        ui.shrink_height_to_current();
         ui.vertical(|ui|{ui.add_space(8.0);});
         ui.horizontal(|ui|{ui.add_space(8.0);});
+        ui.set_min_width(400.0);
 
-        // info!("No tasks found: {:?}", &self.task_data);
-        if let Some(tasks) = &self.task_data{
-            // info!("Found tasks");
-            let task_layout = TaskLayout::default();
-            let _ = task_layout.task_card(tasks, ui);
+        if let Some(tasks) = &mut self.task_data{
+            
+            // ui.allocate_ui(
+            //     Vec2::new(ui.available_size_before_wrap().x, ui.available_size_before_wrap().y),
+            //     |ui| 
+            // {
+                
+                // info!("RECT X//Y: {:?}// {:?}", ui.available_size_before_wrap().x, ui.available_size_before_wrap().y);
+                // info!("SIZE X//Y: {:?}// {:?}", ui.available_size().x, ui.available_size().y);
+            ScrollArea::vertical()
+                .hscroll(false)
+                .show(ui, |ui| 
+            {
+
+                
+                ui
+                    .horizontal_wrapped(|ui| 
+                {
+                    
+                    info!("SIZE X//Y: {:?}// {:?}", ui.available_size().x, ui.available_size().y);
+                    // let max_rect = ui.max_rect();
+                    // info!("Max rect: {:?}", max_rect);
+                
+                    ui.with_layout(Layout::left_to_right(egui::Align::Min).with_main_wrap(true), |ui| {
+                        for task_data in tasks {
+                            let _ = task_data.display_task_cards(ui).unwrap();
+                        }
+                    });
+                });
+            });
         }
     }
 }
