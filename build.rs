d844848f485aeb00111ddf2cc1b7abea67a68b8b
Shadowbroker <darkmage79@gmail.com>
2024-06-10 13:19:21 -0600

    test github action

--- build.rs
+++ build.rs
@@ -6,6 +6,7 @@ fn main() {
     {
         static_vcruntime::metabuild();
         println!("cargo:rerun-if-changed=MasterTech.rc");
+        println!("cargo:rerun-if-changed=build.rs");
         embed_resource::compile("src/assets/MasterTech.rc", embed_resource::NONE);
         // println!("cargo rustc -- -Ctarget-feature=+crt-static");
         // println!("cargo:rustc-link-lib=static=stdc++");
