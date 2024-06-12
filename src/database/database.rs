fd1821d176c061507ea1c9631816a7dcd72bf6d3
Shadowbroker <darkmage79@gmail.com>
2024-06-11 22:31:17 -0600

    more finalizing of changes from merge

--- src/database/database.rs
+++ src/database/database.rs
@@ -38,9 +38,13 @@ impl Database{
             .await.unwrap();
 
         // Select a specific namespace / database
-        database.use_ns("Mastertech").use_db("MastertechDB").await?;
+        database
+            .use_ns("Mastertech")
+            .use_db("MastertechDB")
+            .await
+            .expect("Could not use ns or db name");
 
-        Ok( Self { database } )
+        Database { database }
     }
 
     pub async fn insert<T: Serialize>(&self, table: &str, record: T) -> Result<Vec<Record>, Error> {
