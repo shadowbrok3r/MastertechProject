9cf7a1cd322cefa107615defdec6f43e6d04a6bc
Shadowbroker <darkmage79@gmail.com>
2024-09-04 15:14:17 -0600

    breaking presta API changes, trying to work around this

--- Mastertech4.0/src/tabs/tur_sheet/presta_api.rs
+++ Mastertech4.0/src/tabs/tur_sheet/presta_api.rs
@@ -1,16 +1,25 @@
-use database::schema::{prestashop_schema::{Address, Customer, CustomerMessage, CustomerThread, Employee, Order, PrestashopPayload, SubResource}, CustomerData, CustomerId, CUSTOMER_TABLE};
-use reqwest::{header::{ACCEPT, AUTHORIZATION, CONTENT_TYPE}, Client};
 use crate::app_state::MastertechContext;
+use database::schema::{
+    prestashop_schema::{
+        Address, Customer, CustomerMessage, CustomerThread, Employee, Order, PrestashopPayload,
+        SubResource,
+    },
+    CustomerData, CustomerId, CUSTOMER_TABLE,
+};
+use log::{error, info};
+use reqwest::{
+    header::{ACCEPT, AUTHORIZATION, CONTENT_TYPE},
+    Client,
+};
+use serde::Deserialize;
 use serde_json::{from_value, Value};
 use std::collections::HashMap;
 use surrealdb::sql::Thing;
-use serde::Deserialize;
-use log::{error, info};
 
 const AUTH_TOKEN: &str = "Basic SVAxUlE2UkZSTUZXQjZCOFdIUVY4RFpQV1ZOTDIxWE06";
 
 impl MastertechContext {
-    pub fn presta_api(&mut self){ 
+    pub fn presta_api(&mut self) {
         let input = self.ticket_data.service_number.clone();
         let tx = self.prestashop_api_tx.clone();
         if !input.is_empty() {
@@ -20,90 +29,101 @@ impl MastertechContext {
 
                 query.insert("filter[id_order]", input.as_str());
                 query.insert("output_format", "JSON");
-                
-                let customer_threads: Vec<CustomerThread> = api_call.request_resources(
-                    "customer_threads",
-                    query.clone()
-                ).await.unwrap_or_default();
+
+                let customer_threads: Vec<CustomerThread> = api_call
+                    .request_resources("customer_threads", query.clone())
+                    .await
+                    .unwrap_or_default();
 
                 let mut customer_messages: Vec<CustomerMessage> = Vec::new();
 
-                if !customer_threads.is_empty(){
-                    for thread in customer_threads.iter(){
-                        for msg in thread.associations.customer_messages.iter(){
-                            customer_messages.push(
-                                api_call.request_subresources_by_id(
-                                    "customer_messages", 
+                if !customer_threads.is_empty() {
+                    for thread in customer_threads.iter() {
+                        for msg in thread.associations.customer_messages.iter() {
+                            match api_call
+                                .request_subresources_by_id(
+                                    "customer_messages",
                                     "customer_message",
-                                    msg.id.as_str()
-                                ).await.unwrap_or_default()
-                            );
+                                    msg.id.as_str(),
+                                )
+                                .await
+                            {
+                                Ok(msg) => customer_messages.push(msg),
+                                Err(e) => info!("Error getting customer messages: {e:?}"),
+                            }
                         }
                     }
                 }
 
-                let order: Order = api_call.request_subresources_by_id(
-                    "orders", 
-                    "order", 
-                    &input
-                ).await.unwrap_or_default();
+                let order: Order = api_call
+                    .request_subresources_by_id("orders", "order", &input)
+                    .await
+                    .unwrap();
 
-                if order.id_customer.is_empty(){
+                if order.id_customer.is_empty() {
                     info!("Order is likely gonna fuKKKK");
                 }
 
                 info!("order: {order:#?}");
 
-                let sales_rep: Option<Employee> = if !order.id_employee_sales_rep.contains("0") { //|| order.id_employee_sales_rep.len() != 0{
-                    let employee: Employee = api_call.request_subresources_by_id(
-                        "employees", 
-                        "employee", 
-                        &order.id_employee_sales_rep
-                    ).await.unwrap_or_default();
+                let sales_rep: Option<Employee> = if !order.id_employee_sales_rep.contains("0") {
+                    //|| order.id_employee_sales_rep.len() != 0{
+                    let employee: Employee = api_call
+                        .request_subresources_by_id(
+                            "employees",
+                            "employee",
+                            &order.id_employee_sales_rep,
+                        )
+                        .await
+                        .unwrap();
 
                     info!("employee: {employee:#?}");
                     Some(employee)
-                }else{
+                } else {
                     None
                 };
-                let split_rep: Option<Employee> = if !order.id_employee_split_rep.contains("0"){
-                    let employee_2: Employee = api_call.request_subresources_by_id(
-                        "employees", 
-                        "employee", 
-                        &order.id_employee_split_rep
-                    ).await.unwrap_or_default();
+
+                let split_rep: Option<Employee> = if !order.id_employee_split_rep.contains("0") {
+                    let employee_2: Employee = api_call
+                        .request_subresources_by_id(
+                            "employees",
+                            "employee",
+                            &order.id_employee_split_rep,
+                        )
+                        .await
+                        .unwrap();
 
                     info!("employee: {sales_rep:#?}");
                     Some(employee_2)
-                }else{
+                } else {
                     None
                 };
 
-
-                let cust: Customer = api_call.request_subresources_by_id(
-                    "customers", 
-                    "customer", 
-                    &order.id_customer
-                ).await.unwrap_or_default();
+                let cust: Customer = api_call
+                    .request_subresources_by_id("customers", "customer", &order.id_customer)
+                    .await
+                    .unwrap();
 
                 // info!("customer: {customer:#?}");
 
-                let address: Address = api_call.request_subresources_by_id(
-                    "addresses", 
-                    "address", 
-                    &order.id_address_invoice
-                ).await.unwrap_or_default();
+                let address: Address = api_call
+                    .request_subresources_by_id("addresses", "address", &order.id_address_invoice)
+                    .await
+                    .unwrap();
 
                 // let notes: CustomerThread = api_call.request_subresources_by_id(
-                //     "customer_threads", 
-                //     "customer_thread", 
+                //     "customer_threads",
+                //     "customer_thread",
                 //     &order.id_address_delivery
                 // ).await.unwrap();
 
                 info!("address: {address:#?}");
-                // 2059728
-                let customer = CustomerData{
-                    id: Some(CustomerId(Thing::from((CUSTOMER_TABLE.to_string(), order.id_customer.clone())))),
+
+                let customer = CustomerData {
+                    id: Some(CustomerId(Thing::from((
+                        CUSTOMER_TABLE.to_string(),
+                        order.id_customer.clone(),
+                    )))),
                     cust_code: order.id_customer.clone(),
                     name: format!("{} {}", &cust.firstname, &cust.lastname),
                     phone_number: address.phone.clone().to_string(),
@@ -119,10 +139,10 @@ impl MastertechContext {
                     split_rep,
                     address,
                     customer_threads,
-                    customer_messages
+                    customer_messages,
                 };
 
-                match tx.try_send(presta_payload){
+                match tx.try_send(presta_payload) {
                     Ok(_) => drop(tx),
                     Err(err) => error!("Error: {err:?}"),
                 };
@@ -131,27 +151,27 @@ impl MastertechContext {
     }
 }
 
-pub struct Prestashop<'a>{
+pub struct Prestashop<'a> {
     client: Client,
     /// [field1,field2 …] or 'full'
     display: &'a str,
     /// &schema=synopsis for tests
-    schema: Option<&'a str>, 
-    /** 
-    * [1|5]	    OR operator: list of possible values
-    * [1,10]    Interval operator: define interval of possible values
-    * [John]	Literal value (not case sensitive)
-    * [Jo]%	    Begin operator: fields begins with the value (not case sensitive)
-    * %[hn]	    End operator: fields ends with the value (not case sensitive)
-    * %[oh]%	Contains operator: fields contains the value (not case sensitive)
-    */ 
+    schema: Option<&'a str>,
+    /**
+     * [1|5]	    OR operator: list of possible values
+     * [1,10]    Interval operator: define interval of possible values
+     * [John]	Literal value (not case sensitive)
+     * [Jo]%	    Begin operator: fields begins with the value (not case sensitive)
+     * %[hn]	    End operator: fields ends with the value (not case sensitive)
+     * %[oh]%	Contains operator: fields contains the value (not case sensitive)
+     */
     filter: Option<&'a str>,
     /// number, or starting index (limit from number to the index)
     limit: Option<(i32, i32)>,
     // data_channel: PrestaDataChannel
 }
 
-impl <'a> Default for Prestashop<'a>{
+impl<'a> Default for Prestashop<'a> {
     fn default() -> Self {
         Self {
             client: Client::new(),
@@ -163,14 +183,26 @@ impl <'a> Default for Prestashop<'a>{
     }
 }
 
-impl <'a>Prestashop<'a> {
-    pub fn new<T: for <> Deserialize<'a> + std::fmt::Debug + SubResource>(
-        client: Client, display: &'a str, filter: Option<&'a str>, limit: Option<(i32, i32)>, schema: Option<&'a str>,
-    ) -> Self { Self { client, display, filter, limit, schema } }
+impl<'a> Prestashop<'a> {
+    pub fn new<T: Deserialize<'a> + std::fmt::Debug + SubResource>(
+        client: Client,
+        display: &'a str,
+        filter: Option<&'a str>,
+        limit: Option<(i32, i32)>,
+        schema: Option<&'a str>,
+    ) -> Self {
+        Self {
+            client,
+            display,
+            filter,
+            limit,
+            schema,
+        }
+    }
 
     pub fn query_args(&self, resource_name: &str, url_params: HashMap<&str, &str>) -> String {
         let base_url = format!("https://pclaptops.mojo11.com/api/{}", resource_name);
-        
+
         let mut query_params = vec![];
 
         // Adding `display` parameter
@@ -209,15 +241,17 @@ impl <'a>Prestashop<'a> {
     }
 
     pub async fn request_subresources_by_id<T>(
-        &self, 
-        resource: &str, 
-        name: &str, 
-        id: &str
-    ) 
-        -> anyhow::Result<T, anyhow::Error> where T: for <'de>Deserialize<'de> + std::fmt::Debug
+        &self,
+        resource: &str,
+        name: &str,
+        id: &str,
+    ) -> anyhow::Result<T, anyhow::Error>
+    where
+        T: for<'de> Deserialize<'de> + std::fmt::Debug,
     {
         let url = format!("https://pclaptops.mojo11.com/api/{resource}/{id}?output_format=JSON");
-        let response: Value = self.client 
+        let response: Value = self
+            .client
             .get(url.clone())
             .header(CONTENT_TYPE, "application/json")
             .header(ACCEPT, "application/json")
@@ -235,29 +269,31 @@ impl <'a>Prestashop<'a> {
     }
 
     pub async fn request_resources<T>(
-        &self, 
+        &self,
         resource_name: &str,
-        url_params: HashMap<&str, &str>
-    ) 
-        -> anyhow::Result<Vec<T>, anyhow::Error>
-            where T: for <'de>Deserialize<'de> + std::fmt::Debug
+        url_params: HashMap<&str, &str>,
+    ) -> anyhow::Result<Vec<T>, anyhow::Error>
+    where
+        T: for<'de> Deserialize<'de> + std::fmt::Debug,
     {
         info!(
-            "resource_name: {resource_name:#?}, {url_params:#?}\nURL: {:#?}", 
+            "resource_name: {resource_name:#?}, {url_params:#?}\nURL: {:#?}",
             self.query_args(resource_name, url_params.clone())
         );
-        
-        let response: Value = self.client.get(self.query_args(resource_name, url_params))
+
+        let response: Value = self
+            .client
+            .get(self.query_args(resource_name, url_params))
             .header(AUTHORIZATION, AUTH_TOKEN)
             .send()
             .await?
             .json()
             .await?;
-        
+
         info!("response: {:#?}", response);
         let x: Vec<T> = from_value(response[resource_name].clone())?;
         info!("x: {x:#?}");
-        
+
         Ok(x)
     }
 }
