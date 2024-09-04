ace8b2cd543dfe33eff59fef30ea421c17b0dcb2
Shadowbroker <darkmage79@gmail.com>
2024-09-03 20:00:05 -0600

    fixed up coloring for logging

--- MtechServer2.0/src/tabs/logger/mod.rs
+++ MtechServer2.0/src/tabs/logger/mod.rs
@@ -194,9 +194,15 @@ impl LoggerUi {
                         let string_format = format!("[{}]: {}", level, string);
 
                         let response = match level {
-                            log::Level::Warn => ui.colored_label(Color32::LIGHT_YELLOW, string_format),
-                            log::Level::Error => ui.colored_label(Color32::LIGHT_RED, string_format),
-                            log::Level::Info => ui.colored_label(Color32::LIGHT_GREEN, string_format),
+                            log::Level::Warn => {
+                                ui.colored_label(Color32::LIGHT_YELLOW, string_format)
+                            }
+                            log::Level::Error => {
+                                ui.colored_label(Color32::from_rgb(255, 51, 153), string_format)
+                            }
+                            log::Level::Info => {
+                                ui.colored_label(Color32::from_rgb(51, 255, 189), string_format)
+                            }
                             _ => ui.colored_label(Color32::LIGHT_BLUE, string_format),
                         };
 
@@ -219,12 +225,12 @@ impl LoggerUi {
                                         log::Level::Error => ui.label(
                                             RichText::new(string_format)
                                                 .monospace()
-                                                .color(Color32::LIGHT_RED),
+                                                .color(Color32::from_rgb(255, 51, 153)),
                                         ),
                                         log::Level::Info => ui.label(
                                             RichText::new(string_format)
                                                 .monospace()
-                                                .color(Color32::LIGHT_GREEN),
+                                                .color(Color32::from_rgb(51, 255, 189)),
                                         ),
                                         _ => ui.label(
                                             RichText::new(string_format)
@@ -294,3 +300,4 @@ impl LoggerUi {
 pub fn logger_ui() -> LoggerUi {
     LoggerUi::default()
 }
+
