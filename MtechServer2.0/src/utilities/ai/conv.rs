e0a0b6889b28edcce9c648fe10fb85109d2528ca
Shadowbroker <logan@kingsofalchemy.com>
2024-10-23 17:57:44 -0600

    more ai playground changes

--- MtechServer2.0/src/utilities/ai/conv.rs
+++ MtechServer2.0/src/utilities/ai/conv.rs
@@ -1,99 +1,101 @@
 use super::oa_client::OaClient;
 use super::tools::AiTools;
 use crate::utilities::ai::{chat, gpts};
-use anyhow::{Result, Error};
+use anyhow::{Error, Result};
 use async_openai_wasm::types::{
-	ChatCompletionToolChoiceOption, CreateChatCompletionRequest,
+    ChatChoice, ChatCompletionRequestMessage, ChatCompletionToolChoiceOption,
+    CreateChatCompletionRequest,
 };
 use serde_json::Value;
 
 pub async fn send_user_msg(
-	oa_client: OaClient,
-	ai_tools: AiTools,
-	question: &str,
-) -> Result<String, Error> {
-	let chat_client = oa_client.chat();
-	let model = gpts::MODEL;
+    oa_client: OaClient,
+    ai_tools: AiTools,
+    messages: Vec<ChatCompletionRequestMessage>,
+) -> Result<Vec<ChatChoice>, Error> {
+    let chat_client = oa_client.chat();
+    let model = gpts::MODEL;
 
-	// -- Build messages
-	let messages = vec![chat::user_msg(question)?];
+    // -- Extract tools and rpc_router
+    let rpc_router = ai_tools.router();
+    let tools = Some(ai_tools.chat_tools_clone());
 
-	// -- Extract tools and rpc_router
-	let rpc_router = ai_tools.router();
-	let tools = Some(ai_tools.chat_tools_clone());
+    // -- Exec Chat Request
+    let msg_req = CreateChatCompletionRequest {
+        model: model.to_string(),
+        messages: messages.clone(),
+        tools: tools.clone(),
+        tool_choice: Some(ChatCompletionToolChoiceOption::Auto),
+        ..Default::default()
+    };
+    let chat_response = chat_client.create(msg_req).await?;
+    let first_choice = chat::first_choice(chat_response)?;
 
-	// -- Exec Chat Request
-	let msg_req = CreateChatCompletionRequest {
-		model: model.to_string(),
-		messages: messages.clone(),
-		tools: tools.clone(),
-		tool_choice: Some(ChatCompletionToolChoiceOption::Auto),
-		..Default::default()
-	};
-	let chat_response = chat_client.create(msg_req).await?;
-	let first_choice = chat::first_choice(chat_response)?;
+    // -- If message.content, end early
+    if first_choice.message.content.is_some() {
+        return Ok(vec![first_choice.clone()]);
+    }
 
-	// -- If message.content, end early
-	if let Some(response_content) = first_choice.message.content {
-		return Ok(response_content);
-	}
+    // -- Otherwise, get/call tools/rpc calls and capture the Tool Responses
+    struct ToolResponse {
+        tool_call_id: String,
+        /// Response value of the rpc_router call
+        response: Value,
+    }
+    let mut tool_responses: Vec<ToolResponse> = Vec::new();
 
-	// -- Otherwise, get/call tools/rpc calls and capture the Tool Responses
-	struct ToolResponse {
-		tool_call_id: String,
-		/// Response value of the rpc_router call
-		response: Value,
-	}
-	let mut tool_responses: Vec<ToolResponse> = Vec::new();
+    // For each tool_call, rpc_router call
+    let tool_calls = first_choice.message.tool_calls;
+    for tool_call in tool_calls.iter().flatten() {
+        let tool_call_id = tool_call.id.clone();
+        let fn_name = tool_call.function.name.clone();
+        let params: Value = serde_json::from_str(&tool_call.function.arguments)?;
 
-	// For each tool_call, rpc_router call
-	let tool_calls = first_choice.message.tool_calls;
-	for tool_call in tool_calls.iter().flatten() {
-		let tool_call_id = tool_call.id.clone();
-		let fn_name = tool_call.function.name.clone();
-		let params: Value = serde_json::from_str(&tool_call.function.arguments)?;
+        // Execute with rpc_router
+        let call_result = rpc_router
+            .call_route(None, fn_name, Some(params))
+            .await
+            .unwrap();
+        let response = call_result.value;
 
-		// Execute with rpc_router
-		let call_result = rpc_router.call_route(None, fn_name, Some(params)).await.unwrap();
-		let response = call_result.value;
+        // Add it to the tool_responses
+        tool_responses.push(ToolResponse {
+            tool_call_id,
+            response,
+        });
+    }
 
-		// Add it to the tool_responses
-		tool_responses.push(ToolResponse {
-			tool_call_id,
-			response,
-		});
-	}
+    // -- Make messages mutable for follow-up
+    let mut messages = messages;
 
-	// -- Make messages mutable for follow-up
-	let mut messages = messages;
+    // -- Append the tool calls (send from AI Model)
+    if let Some(tool_calls) = tool_calls {
+        messages.push(chat::tool_calls_msg(tool_calls)?);
+    }
 
-	// -- Append the tool calls (send from AI Model)
-	if let Some(tool_calls) = tool_calls {
-		messages.push(chat::tool_calls_msg(tool_calls)?);
-	}
+    // -- Append the Tool Responses (computed by this code)
+    for ToolResponse {
+        tool_call_id,
+        response,
+    } in tool_responses
+    {
+        messages.push(chat::tool_response_msg(tool_call_id, response)?);
+    }
 
-	// -- Append the Tool Responses (computed by this code)
-	for ToolResponse {
-		tool_call_id,
-		response,
-	} in tool_responses
-	{
-		messages.push(chat::tool_response_msg(tool_call_id, response)?);
-	}
+    // -- Exec second request with tool responses
+    let msg_req = CreateChatCompletionRequest {
+        model: model.to_string(),
+        messages,
+        tools,
+        tool_choice: Some(ChatCompletionToolChoiceOption::Auto),
+        ..Default::default()
+    };
+    let chat_response = chat_client.create(msg_req).await?;
+    // let first_choice = chat::first_choice(chat_response)?;
+    let all_choices = chat::all_choices(chat_response)?;
 
-	// -- Exec second request with tool responses
-	let msg_req = CreateChatCompletionRequest {
-		model: model.to_string(),
-		messages,
-		tools,
-		tool_choice: Some(ChatCompletionToolChoiceOption::Auto),
-		..Default::default()
-	};
-	let chat_response = chat_client.create(msg_req).await?;
-	let first_choice = chat::first_choice(chat_response)?;
+    // -- Get the final response
+    // let content = first_choice.message.content.unwrap();
 
-	// -- Get the final response
-	let content = first_choice.message.content.unwrap();
-
-	Ok(content)
+    Ok(all_choices)
 }
