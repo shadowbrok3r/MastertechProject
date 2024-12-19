5d62464105bf1f090a1c746a5343fd6c252ed230
Logan <darkmage79@gmail.com>
2024-12-19 14:01:31 -0700

    another damn commit for the CI test

--- database/src/lib.rs
+++ database/src/lib.rs
@@ -126,7 +126,7 @@ impl Database {
         let cap = Capabilities::all();
         let config = Config::new().capabilities(cap);
 
-        DATABASE.connect::<Wss>((DB_URL_LOCAL, config)).await?; //(&get_db_url()).await?;(&db_url).await?;
+        DATABASE.connect::<Wss>((DB_URL_DEV, config)).await?; //(&get_db_url()).await?;(&db_url).await?;
         DATABASE.use_ns(NS).use_db(DB).await?;
         // Select a specific namespace / database
         let jwt = DATABASE
