d270b6634e0168cb3d45a9f3868b68654b3c9937
Logan <darkmage79@gmail.com>
2025-01-18 10:01:35 -0700

    need to fix 'shell' portion of the admin console, for some reason it drags the whole programs performance..

--- Mastertech4.0/src/viewports/mod.rs
+++ Mastertech4.0/src/viewports/mod.rs
@@ -1,3 +1,4 @@
+use displays::tabs::webconsole_admin::WebConsoleLayout;
 use eframe::egui::{CentralPanel, Context, ViewportBuilder, ViewportId};
 use std::{sync::{atomic::Ordering, Arc}, time::Duration};
 use crate::app_state::MasterTechApp;
@@ -58,5 +59,52 @@ impl MasterTechApp{
                 }
             });
         }
+
+        if self.context.shared_ctx.web_console_layout.wants_to_undock {
+            let layout = &mut self.context.shared_ctx.web_console_layout;
+            let undock_client = layout.undock_client.clone();
+            for client in self.context.shared_ctx.clients.clone() {
+                let should_we_undock = if let Some(undock) = undock_client.get(&client.connection_string)
+                {
+                    undock
+                } else {
+                    &false
+                };
+
+                if *should_we_undock {
+                    let show_ws_viewport = self.context.show_ws_viewport.clone();
+                    let client_hash = client.connection_string.clone();
+                    let client_friendly_name = client.friendly_name.clone();
+                    let viewport_id = ViewportId::from_hash_of(format!("deferred_viewport_ws_connection {client_hash}"));
+                    let viewport_builder = ViewportBuilder::default()
+                        .with_title(format!("{}", client_friendly_name.unwrap_or(client_hash)))
+                        .with_inner_size([400.0, 500.0]);
+                    
+                    ctx.show_viewport_immediate(
+                        viewport_id,
+                        viewport_builder,
+                        |ctx, _class| 
+                    {
+
+                        CentralPanel::default().show(ctx, |ui| {
+                            
+                            let tx = layout.ui_actions_channel.0.clone();
+                            
+                            ui.horizontal(|ui| WebConsoleLayout::client_headers(ui, tx, &client.clone(), undock_client.clone()));
+                            if let Some(ws_client) =
+                                layout.ws_clients.get_mut(&client.connection_string)
+                            {
+                                ws_client.show(ui);
+                            }
+                        });
+
+                        if ctx.input(|i| i.viewport().close_requested()) {
+                            show_ws_viewport.store(false, Ordering::Relaxed); // Tell parent to close us.
+                        }
+                    });
+                }
+            }
+        }
+
     }
 }
