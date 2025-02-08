327d73af0b7d2e2c217611edaf9b8482297985a1
Shadowbroker <darkmage79@gmail.com>
2025-02-07 20:37:56 -0700

    added some docs to windows api stuff in mastertech

--- Mastertech4.0/src/utilities/windows/mod.rs
+++ Mastertech4.0/src/utilities/windows/mod.rs
@@ -128,3 +128,66 @@ pub fn install_windows_updates() -> Result<()> {
     println!("Process completed.");
     Ok(())
 }
+
+
+
+/*** IUpdateSearcher::Search method (wuapi.h)
+
+* Criterion 	Type 	Allowed operators 	Description
+* Type 	string 	=, != 	Finds updates of a specific type, such as "'Driver'" and "'Software'".
+* DeploymentAction 	string 	= 	Finds updates that are deployed for a specific action, such as an installation or uninstallation that the administrator of a server specifies.
+* 
+* "DeploymentAction='Installation'" finds updates that are deployed for installation on a destination computer. "DeploymentAction='Uninstallation'" depends on the other query criteria.
+* 
+* "DeploymentAction='Uninstallation'" finds updates that are deployed for uninstallation on a destination computer. "DeploymentAction='Uninstallation'" depends on the other query criteria.
+* 
+* If this criterion is not explicitly specified, each group of criteria that is joined to an AND operator implies "DeploymentAction='Installation'".
+* IsAssigned 	int(bool) 	= 	Finds updates that are intended for deployment by Automatic Updates.
+* 
+* "IsAssigned=1" finds updates that are intended for deployment by Automatic Updates, which depends on the other query criteria. At most, one assigned Windows-based driver update is returned for each local device on a destination computer.
+* 
+* "IsAssigned=0" finds updates that are not intended to be deployed by Automatic Updates.
+* BrowseOnly 	int(bool) 	= 	"BrowseOnly=1" finds updates that are considered optional.
+* 
+* "BrowseOnly=0" finds updates that are not considered optional.
+* AutoSelectOnWebSites 	int(bool) 	= 	Finds updates where the AutoSelectOnWebSites property has the specified value.
+* 
+* "AutoSelectOnWebSites=1" finds updates that are flagged to be automatically selected by Windows Update.
+* 
+* "AutoSelectOnWebSites=0" finds updates that are not flagged for Automatic Updates.
+* UpdateID 	string(UUID) 	=, != 	Finds updates for which the value of the UpdateIdentity.UpdateID property matches the specified value. Can be used with the != operator to find all the updates that do not have an UpdateIdentity.UpdateID of the specified value.
+* 
+* For example, "UpdateID='12345678-9abc-def0-1234-56789abcdef0'" finds updates for UpdateIdentity.UpdateID that equal 12345678-9abc-def0-1234-56789abcdef0.
+* 
+* For example, "UpdateID!='12345678-9abc-def0-1234-56789abcdef0'" finds updates for UpdateIdentity.UpdateID that are not equal to 12345678-9abc-def0-1234-56789abcdef0.
+* Note  A RevisionNumber clause can be combined with an UpdateID clause that contains an = (equal) operator. However, the RevisionNumber clause cannot be combined with an UpdateID clause that contains the != (not-equal) operator.
+*  
+* 
+* For example, "UpdateID='12345678-9abc-def0-1234-56789abcdef0' and RevisionNumber=100" can be used to find the update for UpdateIdentity.UpdateID that equals 12345678-9abc-def0-1234-56789abcdef0 and whose UpdateIdentity.RevisionNumber equals 100.
+* RevisionNumber 	int 	= 	Finds updates for which the value of the UpdateIdentity.RevisionNumber property matches the specified value.
+* 
+* For example, "RevisionNumber=2" finds updates where UpdateIdentity.RevisionNumber equals 2.
+* 
+* This criterion must be combined with the UpdateID property.
+* CategoryIDs 	string(uuid) 	contains 	Finds updates that belong to a specified category.
+* IsInstalled 	int(bool) 	= 	Finds updates that are installed on the destination computer.
+* 
+* "IsInstalled=1" finds updates that are installed on the destination computer.
+* 
+* "IsInstalled=0" finds updates that are not installed on the destination computer.
+* IsHidden 	int(bool) 	= 	Finds updates that are marked as hidden on the destination computer.
+* 
+* "IsHidden=1" finds updates that are marked as hidden on a destination computer. When you use this clause, you can set the UpdateSearcher.IncludePotentiallySupersededUpdates property to VARIANT_TRUE so that a search returns the hidden updates. The hidden updates might be superseded by other updates in the same results.
+* 
+* "IsHidden=0" finds updates that are not marked as hidden. If the UpdateSearcher.IncludePotentiallySupersededUpdates property is set to VARIANT_FALSE, it is better to include that clause in the search filter string so that the updates that are superseded by hidden updates are included in the search results. VARIANT_FALSE is the default value.
+* IsPresent 	int(bool) 	= 	When set to 1, finds updates that are present on a computer.
+* 
+* "IsPresent=1" finds updates that are present on a destination computer. If the update is valid for one or more products, the update is considered present if it is installed for one or more of the products.
+* 
+* "IsPresent=0" finds updates that are not installed for any product on a destination computer.
+* RebootRequired 	int(bool) 	= 	Finds updates that require a computer to be restarted to complete an installation or uninstallation.
+* 
+* "RebootRequired=1" finds updates that require a computer to be restarted to complete an installation or uninstallation.
+* 
+* "RebootRequired=0" finds updates that do not require a computer to be restarted to complete an installation or uninstallation.
+***/
