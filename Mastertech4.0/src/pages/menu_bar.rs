6ef98c8b2080de5d79748df4d8d8fb4adb6a5a60
Shadowbroker <logan@kingsofalchemy.com>
2025-03-27 09:02:06 -0600

    working on the displaymodal trait for taskmodal

--- Mastertech4.0/src/pages/menu_bar.rs
+++ Mastertech4.0/src/pages/menu_bar.rs
@@ -415,12 +415,13 @@ impl MasterTechApp {
 
 
 pub fn restart_in_terminal_mode() -> std::io::Result<()> {
+    let current_exe = std::env::current_exe()?;
     #[cfg(target_os = "windows")]
     {
         use std::os::windows::process::CommandExt;
         std::process::Command::new("cmd")
             .arg("/C")
-            .arg(&std::env::current_exe()?)
+            .arg(&current_exe)
             .arg("-t")
             .creation_flags(0x00000010) // CREATE_NEW_CONSOLE flag
             .creation_flags(0x00000008) // DETACHED_PROCESS flag
@@ -428,7 +429,7 @@ pub fn restart_in_terminal_mode() -> std::io::Result<()> {
     }
     #[cfg(not(target_os = "windows"))]
     {
-        Command::new(&current_exe)
+        std::process::Command::new(&current_exe)
             .arg("-t")
             .spawn()?;
         // On Unix-like systems, the process is detached by default when the parent exits
