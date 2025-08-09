525f046d911d377b771931733f5116cd6e42f932
Shadowbroker <darkmage79@gmail.com>
2025-08-09 08:36:30 -0600

    refactor: remove Fish shell type and update command completion logic for PowerShell. in command_shell, we need to explicitly override the tabbing before the text edit with input mut

--- displays/src/ui_tools/mod.rs
+++ displays/src/ui_tools/mod.rs
@@ -19,16 +19,24 @@ const ZSTD_LEVEL: i32 = 9;
 pub fn encode_style(message: &Style) -> anyhow::Result<Vec<u8>> {
     let bincoded = encode_to_vec(message, standard()).context("Failed to serialize buffer")?;
     let compressed = zstd::encode_all(std::io::Cursor::new(&bincoded), ZSTD_LEVEL).context("zstd")?;
-    log::info!("Compressed: {compressed:?}");
+    if cfg!(debug_assertions) {
+        log::warn!("Compressed: {compressed:?}");
+    }
     Ok(compressed.into())
 }
 
 pub fn decode_style(packet: &[u8]) -> anyhow::Result<Style> {
-    log::info!("Got bytes: {packet:?}");
+    if cfg!(debug_assertions) {
+        log::warn!("Got bytes: {packet:?}");
+    }
     let bincoded = zstd::decode_all(packet).context("zstd")?;
-    log::info!("bincoded: {bincoded:?}");
+    if cfg!(debug_assertions) {
+        log::warn!("bincoded: {bincoded:?}");
+    }
     let (message, _) = decode_from_slice(&bincoded, standard()).context("bincode")?;
-    log::info!("style: {message:?}");
+    if cfg!(debug_assertions) {
+        log::warn!("style: {message:?}");
+    }
     Ok(message)
 }
 
