f175f730c65f853e5c88568a85c2a3959729b77a
Shadowbroker <logan@kingsofalchemy.com>
2026-04-01 10:40:18 -0600

    Update workspace members and refactor style handling in MtechServer2.0. Changed member name from Mastertech4.0 to MtechServer2.0 in Cargo.toml. Refactored style setting methods in first_run.rs and mtechserver.rs to use set_global_style instead of set_style for consistency. Updated Cargo.toml files in database and displays to downgrade reqwest dependency from version 0.13 to 0.12, and added jiff dependency in displays. Enhanced WebSocket client handling with conditional compilation for non-WASM targets.

--- MtechServer2.0/src/mtechserver.rs
+++ MtechServer2.0/src/mtechserver.rs
@@ -1,47 +1,51 @@
 use displays::app_state::{AppState, MainPages};
 use crate::app_state::MtechServer;
-use eframe::egui::Context;
+use eframe::egui;
 use log::info;
 
 
 impl eframe::App for MtechServer {
-    fn update(&mut self, ctx: &Context, frame: &mut eframe::Frame) {
-        // Branch out all the different crossbeam channels to receive
-        // in their own methods to clean up a lot of boilerplate code
-        // as well as being able to find specific code a lot easier
+    fn logic(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
         self.receive(frame, ctx);
-        self.shared_ctx.menu_bar(ctx); // , &mut self.open_tabs, &mut self.tree
 
-        // Handle changes to state from various places, such as
-        // hitting the login button, clicking the 'home page' button
-        // (which is clicking Mtechserver in the top middle of the page),
-        // if session cookie expires (gets checked in the first_run method),
-        // if manually logged out, etc
+        if let AppState::NoAuth(reason) = &self.shared_ctx.state {
+            if reason.contains("Already connected") {
+                info!("Already connected");
+                let usr = self.shared_ctx.current_user.clone();
+                if let Some(user) = usr {
+                    self.shared_ctx.load_data(ctx, &user);
+                    let _ = self.shared_ctx.app_state_tx.try_send(AppState::Authenticated(MainPages::Tasks));
+                } else {
+                    self.shared_ctx.first_run = true;
+                    self.first_run(ctx, frame);
+                    log::error!("1");
+                    self.shared_ctx.state = AppState::NoAuth("No user detected".to_string());
+                }
+            }
+        }
+    }
+
+    fn ui(&mut self, ui: &mut egui::Ui, _frame: &mut eframe::Frame) {
+        ui.options_mut(|options| {
+            options.max_passes = std::num::NonZeroUsize::new(2).unwrap();
+        });
+
+        let ctx = ui.ctx().clone();
+        self.shared_ctx.menu_bar(&ctx);
+
         match &self.shared_ctx.state {
-            AppState::Authenticated(MainPages::Downloads) => self.shared_ctx.downloads_page(ctx),
-            AppState::Authenticated(MainPages::UserPreferences) => self.shared_ctx.account_settings_page(ctx, self.shared_ctx.app_state_tx.clone()),
-            AppState::Authenticated(_) => self.shared_ctx.main_page(ctx),
+            AppState::Authenticated(MainPages::Downloads) => self.shared_ctx.downloads_page(&ctx),
+            AppState::Authenticated(MainPages::UserPreferences) => self.shared_ctx.account_settings_page(&ctx, self.shared_ctx.app_state_tx.clone()),
+            AppState::Authenticated(_) => self.shared_ctx.main_page(&ctx),
             AppState::CreateAccount => self.shared_ctx.signup_page(
-                ctx,
+                &ctx,
                 self.shared_ctx.db_tx.clone(),
                 self.shared_ctx.app_state_tx.clone(),
             ),
             AppState::NoAuth(reason) => {
-                if reason.to_string().contains("Already connected") {
-                    info!("Already connected");
-                    let usr = self.shared_ctx.current_user.clone();
-                    if let Some(user) = usr {
-                        self.shared_ctx.load_data(ctx, &user);
-                        let _ = self.shared_ctx.app_state_tx.try_send(AppState::Authenticated(MainPages::Tasks));
-                    } else {
-                        self.shared_ctx.first_run = true;
-                        self.first_run(ctx, frame);
-                        log::error!("1");
-                        self.shared_ctx.state = AppState::NoAuth("No user detected".to_string());
-                    }
-                } else {
+                if !reason.contains("Already connected") {
                     self.shared_ctx.login_page(
-                        ctx,
+                        &ctx,
                         self.shared_ctx.db_tx.clone(),
                         self.shared_ctx.app_state_tx.clone(),
                     )
@@ -49,4 +53,6 @@ impl eframe::App for MtechServer {
             }
         }
     }
+
+    fn persist_egui_memory(&self) -> bool { true }
 }
